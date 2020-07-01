4e2139a356f5661c18ab26c113ec8bb6ff699321
azzaare <jf@baffier.fr>
2020-07-01 16:55:28 +0900

    Fixed requirement for Registrator.jl and cleanup

---
+++ README.md
@@ -0,0 +1,7 @@
+# BibParser.jl
+
+BibParser is a Julia package for parsing different bibliographic formats. The output are entries following the BibInternal.jl srtuctures.
+
+Currently, only a BibTeX parser is available.
+
+This package is not meant to be used on its own. Please check [Bibliography.jl]([https://](https://github.com/Azzaare/Bibliography.jl)) for a package handling both import/export from various bibliographic formats.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using BibParser, BibParser.BibTeX
 using Test
 
-for file in ["test.bib", "test2.bib"]
+for file in ["test.bib"]
     println("\nstart $file")
     parsed = parse_file("../examples/$file")
 
@@ -10,7 +10,7 @@ for file in ["test.bib", "test2.bib"]
     for e in parsed[1]
         println("|--->type: $(typeof(e)), key: $(e.id)")
         for fn in fieldnames(typeof(e))
-            println("|     |----$(string(fn)) -> metaprogramming to hard")
+            println("|     |----$(string(fn)) -> metaprogramming to hard") # TODO: better printing
         end
         for (n, v) in pairs(e.other)
             println("|     |----$n -> $v")
@@ -18,9 +18,5 @@ for file in ["test.bib", "test2.bib"]
         println("|")
     end
     @test parsed[2] == :ok
-    println(parsed[1])
 end
-
-# write("actions.dot", Automa.machine2dot(BibParser.bibtex_machine))
-# run(`dot -Tsvg -o actions.svg actions.dot`)
 
