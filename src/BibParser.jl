e9f90f996002d20b87d28a5b0c1b9761c44d6fbc
mcmarius <mcmarius@users.noreply.github.com>
2021-09-03 03:16:28 +0300

    Add CFF support

--- src/BibParser.jl
+++ src/BibParser.jl
@@ -13,11 +13,19 @@ import .BibTeX
 include("csl.jl")
 import .CSL
 
+# CFF module
+include("cff.jl")
+import .CFF
+
 """
     parse_file(path::String; parser::Symbol = :BibTeX)
 Parse a bibliography file. Default to BibTeX format. No other options available yet (CSL-JSON coming soon).
 """
-parse_file(path; parser=:BibTeX) = parser == :BibTeX && BibTeX.parse_file(path)
+function parse_file(path; parser=:BibTeX)
+    parser == :BibTeX && return BibTeX.parse_file(path)
+    parser == :CFF && return CFF.parse_file(path)
+end
+
 
 """
     parse_entry(entry::String; parser::Symbol = :BibTeX)
