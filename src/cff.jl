3a39a3d0e77cfb8f74ad4c8735bfca01aca9c4d0
mcmarius <mcmarius@users.noreply.github.com>
2021-09-03 20:39:55 +0300

    Tests, fixes, refactor, docs

--- src/cff.jl
+++ src/cff.jl
@@ -2,34 +2,54 @@ module CFF
 
 using Dates
 using BibInternal
+using BibParser
 using JSONSchema: Schema, validate
 using YAML
 
-import ..Utils
-
 export parse_file
 
-struct UnsupportedCFFVersion <: Any
-end
 
 const CFF_VERSIONS = Set(["1.2.0"])
+const HELP_URL     = "https://github.com/citation-file-format/citation-file-format/blob/main/schema-guide.md"
+const PACKAGE_ROOT = pkgdir(BibParser)
 
+"""
+Simple error struct used to abort parsing if the CFF version is not supported or invalid.
+"""
+struct UnsupportedCFFVersion <: Any
+    version::String
+end
+
+"""
+    parse_file(path::String; id::String = "") ->
+        Tuple{
+            Union{
+                BibInternal.Entry,
+                UnsupportedCFFVersion,
+                YAML.ParserError,
+                Any
+              },
+            Bool
+        }
+
+Parse a CFF file located at `path`. Return a tuple with result and boolean status.
+On success, a BibInternal.Entry is returned. On failure, the error object is returned.
+"""
 function parse_file(path; id = "")
     try
         content = YAML.load_file(path; dicttype=Dict{String, Any})
         cff_version = content["cff-version"]
         if !(cff_version in CFF_VERSIONS)
-            throw(UnsupportedCFFVersion())
+            throw(UnsupportedCFFVersion(cff_version))
         end
         # TODO cache schemas
-        schema = Schema(read(joinpath("src", "cff", "schema-" * cff_version * ".json"), String))
+        schema = Schema(read(joinpath(PACKAGE_ROOT, "src", "cff", "schema-$(cff_version).json"), String))
         errors = validate(content, schema)
-        if errors === nothing
-            content
-        else
-            println(errors)
+        if !isnothing(errors)
+            @warn "Invalid CFF file; see schema guide for details: $(HELP_URL)"
+            @debug errors
 
-            false
+            return errors, false
         end
 
         if haskey(content, "preferred-citation")
@@ -51,25 +71,36 @@ function parse_file(path; id = "")
 
         BibInternal.Entry(
             access, names, booktitle, date, editors, eprint, id, in_, fields, title, type_
-        )
+        ), true
     catch err
         if isa(err, YAML.ParserError)
             print("Parse error invalid YAML: ")
         end
-        println(err)
+        @warn err
 
-        false
+        err, false
     end
 end
 
+"""
+    add_names(content::Dict{String, Any}) -> Vector{BibInternal.Name}
+"""
 function add_names(content)
     [parse_name(author) for author in content["authors"]]
 end
 
+"""
+    add_access(content::Dict{String, Any}) -> BibInternal.Access
+"""
 function add_access(content)
     doi = get(content, "doi", "")
-    if haskey(content, "identifiers") && !isempty(content["identifiers"])
-        doi = content["identifiers"][1]["doi"]
+    if haskey(content, "identifiers")
+        for identifier in content["identifiers"]
+            if get(identifier, "type", "") == "doi"
+                doi = identifier["value"]
+                break
+            end
+        end
     end
     url = get(content, "repository-code", "")
     if isempty(url) && haskey(content, "url")
@@ -79,6 +110,9 @@ function add_access(content)
     BibInternal.Access(doi, "", url)
 end
 
+"""
+    add_date(content::Dict{String, Any}) -> BibInternal.Date
+"""
 function add_date(content)
     date = get(content, "date-released", "")
     if isempty(date)
@@ -87,22 +121,28 @@ function add_date(content)
 
     date = Date(date, dateformat"yyyy-mm-dd")
 
-    BibInternal.Date(Dates.day(date), Dates.month(date), Dates.year(date))
+    BibInternal.Date(string(Dates.day(date)), string(Dates.month(date)), string(Dates.year(date)))
 end
 
+"""
+    add_eprint(content::Dict{String, Any}) -> BibInternal.Eprint
+"""
 function add_eprint(content)
-    # need to determine what conventions are widely used in CFFs for this
+    # need to determine what conventions are widely used in CFFs for this field
     # this is not implemented by other converters (yet?)
     BibInternal.Eprint("", "", "")
 end
 
+"""
+    generate_id(names::Vector{BibInternal.Name}, title::String, year::String, doi::String) -> String
+"""
 function generate_id(names, title, year, doi)
     separator = "-"
     dash_title = replace(title, " " => separator)[1:5]
     names_prefix = join(
         map(
             x -> replace(x, " " => separator),
-            map(Utils.name_to_string, first(names, 2))
+            map(BibParser.Utils.name_to_string, first(names, 2))
         ),
         separator
     )
@@ -111,14 +151,18 @@ function generate_id(names, title, year, doi)
         # For CFF 1.2.0, only author and title are required
         # this avoids collisions at the cost of readability
         date_str = Dates.format(Dates.now(), dateformat"YYYY-mm-ddTH:MM:SS.sss")
-        prefix * date_str
+        "$(prefix)$(separator)$(date_str)"
     else
-        prefix * doi
+        "$(prefix)$(separator)$(doi)"
     end
 end
 
+"""
+    add_in(content::Dict{String, Any}) -> BibInternal.In
+"""
 function add_in(content)
     address = ""
+    # there is no top-level address, so this might not be suitable
     if !isempty(content["authors"])
         for author in content["authors"]
             if haskey(author, "address")
@@ -129,23 +173,23 @@ function add_in(content)
     end
     start  = get(content, "start", "")
     finish = get(content, "end", "")
-    pages = start == finish || isempty(finish) ? start : "{" * start * "--" * finish * "}"
+    pages = start == finish || isempty(finish) ? start : "{$(start)--$(finish)}"
     publisher = get(content, "publisher", Dict())
     publisher = get(publisher, "name", "")
 
     BibInternal.In(
         address,
-        "",                         # chapter
-        "",                         # edition
-        "",                         # institution
+        "",                                # chapter
+        "",                                # edition
+        "",                                # institution
         get(content, "journal", ""),
-        get(content, "issue", ""),  # number
-        "",                         # organization
+        string(get(content, "issue", "")), # number
+        "",                                # organization
         pages,
         publisher,
-        "",                         # school
-        "",                         # series
-        get(content, "volume", "")
+        "",                                # school
+        "",                                # series
+        string(get(content, "volume", ""))
     )
 end
 
@@ -164,10 +208,16 @@ const CFF_TO_BIBTEX_TYPES = Dict{String, String}(
     ]
 )
 
+"""
+    add_type(content::Dict{String, Any}) -> String
+"""
 function add_type(content)
     get(CFF_TO_BIBTEX_TYPES, content["type"], "misc")
 end
 
+"""
+    parse_name(Dict{String, Any} -> BibInternal.Name
+"""
 function parse_name(author)
     if haskey(author, "name")  # note: cffconvert does not treat this case
         BibInternal.Name("", author["name"], "", "", "")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,38 @@
 using BibParser, BibParser.BibTeX
 using Test
 
-for file in ["test.bib", "error.bib"]
-    println("\nstart $file")
-    parsed = parse_file("../examples/$file")
+const PACKAGE_ROOT = pkgdir(BibParser)
 
-    for (k, e) in pairs(parsed)
-        println("|--->$(e.id)")
-        for fn in fieldnames(typeof(e))
-            println("|     |----$(string(fn)) -> parsed!") # TODO: better printing
+function test_bibtex()
+    for file in ["test.bib", "error.bib"]
+        println("\nstart $file")
+        parsed = parse_file(joinpath(PACKAGE_ROOT, "examples", "$file"))
+
+        for (k, e) in pairs(parsed)
+            println("|--->$(e.id)")
+            for fn in fieldnames(typeof(e))
+                println("|     |----$(string(fn)) -> parsed!") # TODO: better printing
+            end
+            for (n, v) in pairs(e.fields)
+                println("|     |----$n -> $v")
+            end
+            println("|")
         end
-        for (n, v) in pairs(e.fields)
-            println("|     |----$n -> $v")
-        end
-        println("|")
     end
 end
+
+function test_cff()
+    for file in ["CITATION.cff", "invalid_yaml.cff", "invalid_version.cff", "invalid_schema.cff"]
+        @info "Start $file"
+        parsed, result = parse_file(joinpath(PACKAGE_ROOT, "examples", file), :CFF)
+        if result
+            @info "OK"
+        else
+            @info "Error"
+        end
+    end
+end
+
+
+test_bibtex()
+test_cff()
