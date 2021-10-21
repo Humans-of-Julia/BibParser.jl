f2ee5520436c69128e0e59dac344624580a37f40
azzaare <jf@baffier.fr>
2021-10-21 14:55:02 +0900

    Add flexible rules check. Tag new version. Enforce Julia ^1.6 compat

--- src/BibParser.jl
+++ src/BibParser.jl
@@ -7,32 +7,34 @@ include("utils.jl")
 
 # BibTeX module
 include("bibtex.jl")
-import .BibTeX
+using .BibTeX: BibTeX
 
 # CSL-JSON module
 include("csl.jl")
-import .CSL
+using .CSL: CSL
 
 # CFF module
 include("cff.jl")
-import .CFF
+using .CFF: CFF
 
 """
     parse_file(path::String; parser::Symbol = :BibTeX)
 Parse a bibliography file. Default to BibTeX format. Other options available: CFF (CSL-JSON coming soon).
+For bibliography formats with formatting rules (such as `:BibTeX`), the `check` keyword argument can be set to `:none` (or `nothing`), `:warn`, or `:error`.
 """
-parse_file(path, ::Val{:BibTeX})   = BibTeX.parse_file(path)
-parse_file(path, ::Val{:CFF})      = CFF.parse_file(path)
+parse_file(path, ::Val{:BibTeX}; check) = BibTeX.parse_file(path; check)
+parse_file(path, ::Val{:CFF}; check) = CFF.parse_file(path)
 
-parse_file(path, parser = :BibTeX) = parse_file(path, Val(parser))
-
+parse_file(path, parser=:BibTeX; check=:error) = parse_file(path, Val(parser); check)
 
 """
     parse_entry(entry::String; parser::Symbol = :BibTeX)
 Parse a string entry. Default to BibTeX format. No other options available yet (CSL-JSON coming soon).
+
+For bibliography formats with formatting rules (such as `:BibTeX`), the `check` keyword argument can be set to `:none` (or `nothing`), `:warn`, or `:error`.
 """
-function parse_entry(entry; parser=:BibTeX)
-    return parser == :BibTeX && return BibTeX.parse_string(entry)
+function parse_entry(entry; parser=:BibTeX, check = :error)
+    return parser == :BibTeX && return BibTeX.parse_string(entry; check)
 end
 
 end # module
